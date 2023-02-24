4f3f341f11ece5c380c61e22110ddcd380d9b67c
penpizza <110964198+penpizza@users.noreply.github.com>
2023-02-24 22:28:06 +0700

    completed

--- lab21.h
+++ lab21.h
@@ -63,13 +63,42 @@ void Unit::newTurn(){
 	guard_on = false;
 }
 
+bool Unit::isDead(){
+    if (hp<=0) return true;
+    else return false;
+}
 
+void Unit::guard(){
+    guard_on = true;
+}
 
-/////////////////////////////////////////////////////////////////////////////////////
-//Write function members isDead(), guard(), heal(), beAttacked(), and attack() here//
-/////////////////////////////////////////////////////////////////////////////////////
+int Unit::beAttacked(int oppatk){
+	int dmg = oppatk-def;
+	if(dmg <= 0){
+		dmg = 0;
+	}else{
+		if(guard_on == true) dmg = dmg/3.0;
+	}
+	hp = hp-dmg;
+	return dmg;
+}
 
+int Unit::attack(Unit &name){
+	return name.beAttacked(atk);
+}
 
+int Unit::heal(){
+	int heal = rand()%21+10;
+	if(hpmax- hp >= heal){
+		hp +=heal;
+		return heal;
+	}else{
+	    heal = hpmax-hp;
+		hp = hpmax;
+		return heal;
+	}
+
+}
 
 void drawScene(char p_action,int p,char m_action,int m){
 	cout << "                                                       \n";
